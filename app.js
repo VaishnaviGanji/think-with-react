22e5e2d1481019ab7c9dd7635a95cbd67c1572cd
VaishnaviGanji <vaishnaviganji24@gmail.com>
2021-02-16 20:54:01 +0530

    1578196

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+class Container extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                    Hey ProGrads! Welcome to React Learning 
+                </div>
+                <div>
+                    Let's rock and roll
+                </div>
+            </div>
+        );
+    }
+}
+
 const container = document.getElementById('react-container');
-ReactDOM.render("Hello! Welcome to React",container);
+ReactDOM.render(<Container />,container);
